fc9e4ec0dd7420b4eaf545272b660cefcc15758a
Zsailer <zachsailer@gmail.com>
2018-10-17 19:35:48 -0700

    add markdown context type to setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,12 @@ with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='traitlets_paths',
-    version='0.1.2',
+    version='0.1.1',
     author='Zach Sailer',
     author_email='zachsailer@gmail.com',
     description='Traitlets module for pathlib.',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/Zsailer/traitlets_paths',
     packages=find_packages(exclude=('tests',)),
     install_requires=['traitlets'],
