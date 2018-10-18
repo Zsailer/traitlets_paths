9c46d5b739b28b7c52c85d973c8c77a1b8308aae
Zsailer <zachsailer@gmail.com>
2018-10-17 17:47:55 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='traitlets_paths',
-    version='0.1.0',
+    version='0.1.1',
     author='Zach Sailer',
     author_email='zachsailer@gmail.com',
     description='Traitlets module for pathlib.',
