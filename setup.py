2f39320131b92eaad0a0900dfac32c0c8b3f42f4
Zsailer <zachsailer@gmail.com>
2018-10-17 19:32:11 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
+import os
+import io
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
 
 setup(
     name='traitlets_paths',
-    version='0.1.1',
+    version='0.1.2',
     author='Zach Sailer',
     author_email='zachsailer@gmail.com',
     description='Traitlets module for pathlib.',
+    long_description=long_description,
+    url='https://github.com/Zsailer/traitlets_paths',
     packages=find_packages(exclude=('tests',)),
     install_requires=['traitlets'],
     classifiers=[
